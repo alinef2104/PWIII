802c5e205383e8be5659edad2e30f66b2a04d167
alinef2104 <alinefcordeiro04@gmail.com>
2025-02-11 14:52:28 -0300

    commite legall

---
+++ pikachu.php
@@ -0,0 +1,37 @@
+<?php
+  include './header.php';
+?>
+
+<div class="conteiner">
+  <div class="row d-flex justify-content-center">
+      <div class="col-10 p-3">
+              <div id="carouselExampleControlsNoTouching" class="carousel slide" data-bs-touch="false">
+            <div class="carousel-inner">
+              <div class="carousel-item active">
+                <img src="https://preview.redd.it/prwa3v3e7cu91.jpg?auto=webp&s=dd2e66591af0c0f2578514dbb9a66ae349dd0ba5" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://platform.polygon.com/wp-content/uploads/sites/2/chorus/uploads/chorus_asset/file/24458108/captain_pikachu.jpg?quality=90&strip=all&crop=0,3.4613147178592,100,93.077370564282" class="d-block w-100" alt="...">
+              </div>
+              <div class="carousel-item">
+                <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQW2FKanQQLZVW0OBkS2ScJmwZW3gC9Y8J5aw&s" class="d-block w-100" alt="...">
+              </div>
+            </div>
+            <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="prev">
+              <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Previous</span>
+            </button>
+            <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleControlsNoTouching" data-bs-slide="next">
+              <span class="carousel-control-next-icon" aria-hidden="true"></span>
+              <span class="visually-hidden">Next</span>
+            </button>
+          </div>
+      </div>
+  </div>
+</div>
+
+
+
+<?php
+include './footer.php';
+?>
